d195d219ba2717b9670be615cea397385aef4577
Graham-Stein <graham@guavoaviation.co.uk>
2018-09-28 11:57:52 +0100

    Add create methods to all classes

--- console.rb
+++ console.rb
@@ -2,3 +2,29 @@ require('pry')
 require_relative('models/customers.rb')
 require_relative('models/films.rb')
 require_relative('models/tickets.rb')
+
+
+
+customer1 = Customer.new({
+  'name' => 'John Wayne',
+  'funds' => 100
+  })
+
+customer1.save
+
+film1 = Film.new({
+  'title' => 'Back to the Future',
+  'price' => 10
+  })
+
+film1.save
+
+ticket1 = Ticket.new({
+  'customer_id' => customer1.id,
+  'film_id' => film1.id
+  })
+
+ticket1.save
+
+binding.pry
+nil
--- models/films.rb
+++ models/films.rb
@@ -1,1 +1,33 @@
 require_relative("../db/sql_runner")
+
+
+
+class Film
+
+  attr_reader :id
+  attr_accessor :title, :price
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @title = options['title']
+    @price = options['price'].to_i
+  end
+
+  def save()
+    sql = "
+      INSERT INTO films(
+        title,
+        price
+      )
+      VALUES (
+        $1,
+        $2
+      )
+      RETURNING id;"
+    values = [@title, @price]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i
+  end
+
+
+end
